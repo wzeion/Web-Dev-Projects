69dc621ce545a197a8f1f919664d28e26d4e8a61
Ayan Nath <ayanjustwon@gmail.com>
2025-02-16 22:42:28 +0530

    firstCommit

---
+++ Dark-Theme-Sunstone/content.js
@@ -0,0 +1,3 @@
+document.documentElement.style.filter = "invert(1) hue-rotate(180deg)";
+
+//We tried to revert the effect for the images but it wasn't possible as the whole page is dynamically rendered.
